43dfa342d9292499b2a32a67c4b0fce38964a5e5
Jaka Stavanja <jakastavanja@gmail.com>
2016-05-07 16:05:22 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -27,6 +27,7 @@ streznik.use(
 );
 
 var razmerje_usd_eur = 0.877039116;
+var notFirst = false;
 
 function davcnaStopnja(izvajalec, zanr) {
   switch (izvajalec) {
@@ -201,7 +202,9 @@ streznik.post('/prijava', function(zahteva, odgovor) {
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
     var napaka2 = false;
+    notFirst = true;
     try {
+      
       var stmt = pb.prepare("\
         INSERT INTO Customer \
     	  (FirstName, LastName, Company, \
@@ -209,12 +212,12 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State , polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
+    odgovor.redirect('/prijava');
     odgovor.end();
   });
 })
@@ -223,7 +226,18 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        
+        // prvo odprtje
+        if(!notFirst) {
+          odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni}); 
+        } else {
+          if(napaka2) {
+            odgovor.render('prijava', {sporocilo: "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.", seznamStrank: stranke, seznamRacunov: racuni}); 
+          } else {
+            odgovor.render('prijava', {sporocilo: "Stranka je bila uspešno registrirana.", seznamStrank: stranke, seznamRacunov: racuni}); 
+          }
+        }
+         
       }) 
     });
 })
